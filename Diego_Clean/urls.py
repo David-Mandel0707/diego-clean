5af331db534c527e2e8e3caa1f6687b460fbb43b
jpaveiro <jpaveiros@gmail.com>
2026-04-20 15:23:43 -0300

    wip: login route

--- Diego_Clean/urls.py
+++ Diego_Clean/urls.py
@@ -16,12 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.shortcuts import redirect
 
 urlpatterns = [
-    path('', lambda request: redirect('accounts/')),
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
-    path('diretores/', include('diretores.urls')),
-    path('vendedores/',include('vendedores.urls'))
+    # path('diretores/', include('diretores.urls')),
+    # path('vendedores/',include('vendedores.urls'))
 ]
