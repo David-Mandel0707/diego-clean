8856208686ec8e7ba5881bd5af908a1b17fd416b
Negresco_Felipe <felipenegreirossouza@gmail.com>
2026-04-21 12:59:38 -0300

    Remove unused import in views.py

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-
+from django.shortcuts import render
 
 def login(request):
     if request.user.is_authenticated:
