1303511c0d02464df0479addd5db1acbf5d84316
Negresco_Felipe <felipenegreirossouza@gmail.com>
2026-04-22 10:58:27 -0300

    Refactor login function to handle authentication

--- accounts/views.py
+++ accounts/views.py
@@ -4,6 +4,19 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 def login(request):
-    if request.user.is_authenticated:
-        return HttpResponse("Site funcionando")
-        return redirect('/accounts/login/')  
+     if request.user.is_authenticated:
+        return redirect('home')
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            auth_login(request, user)
+            return redirect('home')
+        else:
+            return render(request, 'login.html', {'error': 'Usuário ou senha inválidos'})
+
+    return render(request, 'login.html')
