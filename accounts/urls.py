0868f79c71a82a34b722301faf3a93432e809149
jpaveiro <jpaveiros@gmail.com>
2026-04-23 13:24:11 -0300

    fix: arruma erros de falta de import e sintaxe

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from . import views
+from django.contrib import admin
+from django.urls import include
 
 urlpatterns = [
     path('', views.login, name='login'),
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('clientes/', views.lista_clientes, name='lista_clientes'),
-]
     path('login/', views.login, name='login'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from .models import cliente
+from django.contrib.auth import authenticate, login
 
 
 def login(request):
@@ -16,7 +17,7 @@ def login(request):
         user = authenticate(request, username=username, password=password)
 
         if user is not None:
-            auth_login(request, user)
+            login(request, user)
             return redirect('Home.html')
         else:
             return render(request, 'login.html', {'error': 'Usuário ou senha inválidos'})
