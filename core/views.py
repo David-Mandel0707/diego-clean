49b28f6f6d3995d5c68926bc03745abf5850c52a
Negresco_Felipe <felipenegreirossouza@gmail.com>
2026-04-23 10:42:27 -0300

    Add lista_clientes view to list all clients

--- core/views.py
+++ core/views.py
@@ -2,3 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpRequest
 from django.contrib.auth.decorators import login_required
 from .models import Cliente
+
+def lista_clientes(request):
+    clientes = Cliente.objects.all()
+    return render(request, 'Home.html', {'clientes': clientes})
