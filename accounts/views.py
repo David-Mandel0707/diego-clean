4c9ff52c829baa481ef38072bebfb0392412835e
Negresco_Felipe <felipenegreirossouza@gmail.com>
2026-04-23 12:52:38 -0300

    Refactor login function to login_view and update redirects

--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
-from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login as auth_login
 from .models import cliente
 
-def login(request):
-     if request.user.is_authenticated:
-        return redirect('Home.html')
+def login_view(request):
+    if request.user.is_authenticated:
+        return redirect('home')
 
     if request.method == 'POST':
         username = request.POST.get('username')
@@ -16,17 +15,13 @@ def login(request):
 
         if user is not None:
             auth_login(request, user)
-            return redirect('Home.html')
+            return redirect('home')
         else:
             return render(request, 'login.html', {'error': 'Usuário ou senha inválidos'})
 
     return render(request, 'login.html')
 
+
 def lista_clientes(request):
     clientes = cliente.objects.all()
     return render(request, 'clientes.html', {'clientes': clientes})
-    if request.user.is_authenticated:
-        return HttpResponse("Site funcionando")
-    if request.method=='POST':
-        return HttpResponse("Site funcionando")
-    return render(request, 'login.html')  
