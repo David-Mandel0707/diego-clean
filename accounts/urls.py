d7d03add6057e1b626adfece95a0b040984e3037
Negresco_Felipe <felipenegreirossouza@gmail.com>
2026-04-22 22:12:07 -0300

    Add route for lista_clientes view

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.login, name='login'),
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
+    path('clientes/', views.lista_clientes, name='lista_clientes'),
 ]
