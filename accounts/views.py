e68bc2394199f15ee1787db7876721dc8016908d
Negresco_Felipe <felipenegreirossouza@gmail.com>
2026-04-23 12:58:54 -0300

    Refactor login view and simplify response handling

--- accounts/views.py
+++ accounts/views.py
@@ -1,27 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login as auth_login
-from .models import cliente
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
 
-def login_view(request):
+def login(request):
     if request.user.is_authenticated:
-        return redirect('home')
-
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-
-        user = authenticate(request, username=username, password=password)
-
-        if user is not None:
-            auth_login(request, user)
-            return redirect('home')
-        else:
-            return render(request, 'login.html', {'error': 'Usuário ou senha inválidos'})
-
-    return render(request, 'login.html')
-
-
-def lista_clientes(request):
-    clientes = cliente.objects.all()
-    return render(request, 'clientes.html', {'clientes': clientes})
+        return HttpResponse("Site funcionando")
+    if request.method=='POST':
+        return HttpResponse("Site funcionando")
+    return render(request, 'login.html')  
