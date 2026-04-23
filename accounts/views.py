14a9b0d479120f3f3717a6f2758a3e67597e6113
Negresco_Felipe <felipenegreirossouza@gmail.com>
2026-04-22 22:10:04 -0300

    Add lista_clientes function to view all clients

--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from .models import cliente
 
 def login(request):
      if request.user.is_authenticated:
@@ -20,3 +21,7 @@ def login(request):
             return render(request, 'login.html', {'error': 'Usuário ou senha inválidos'})
 
     return render(request, 'login.html')
+
+def lista_clientes(request):
+    clientes = cliente.objects.all()
+    return render(request, 'clientes.html', {'clientes': clientes})
