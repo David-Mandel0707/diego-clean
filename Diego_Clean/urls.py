8b3b2e27c85ee0512b9675e4093c7eb96d2ad6c6
David-Mandel0707 <dvdmndel@gmail.com>
2026-04-23 18:56:12 -0300

    Redirect automatico ao abrir a pagina

--- Diego_Clean/urls.py
+++ Diego_Clean/urls.py
@@ -9,6 +9,8 @@ urlpatterns = [
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('reset_done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 
+    path('', lambda _: redirect('/login/')),
+    
     path('home/', include('core.urls')),
     path('login/', include('accounts.urls')),
     path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
