523b4d0e286d3d7c9cec4cb5fe0e37e4261efeaf
Negresco_Felipe <felipenegreirossouza@gmail.com>
2026-04-23 13:07:11 -0300

    Update views.py

--- accounts/views.py
+++ accounts/views.py
@@ -2,10 +2,36 @@ from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from .models import cliente
+
 
 def login(request):
     if request.user.is_authenticated:
+        return redirect('Home.html')
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            auth_login(request, user)
+            return redirect('Home.html')
+        else:
+            return render(request, 'login.html', {'error': 'Usuário ou senha inválidos'})
+
+    return render(request, 'login.html')
+
+
+def lista_clientes(request):
+    clientes = cliente.objects.all()
+    return render(request, 'clientes.html', {'clientes': clientes})
+
+    if request.user.is_authenticated:
         return HttpResponse("Site funcionando")
-    if request.method=='POST':
+
+    if request.method == 'POST':
         return HttpResponse("Site funcionando")
-    return render(request, 'login.html')  
+
+    return render(request, 'login.html')
