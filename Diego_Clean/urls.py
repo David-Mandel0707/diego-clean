9d3f22782cf6e201e736ad32795ec52d3986f8e0
David-Mandel0707 <dvdmndel@gmail.com>
2026-04-23 19:00:26 -0300

    Adicionar redirect shorctut

--- Diego_Clean/urls.py
+++ Diego_Clean/urls.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import views as auth_views
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
